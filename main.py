ddd68e5265a9b6c187285339a897d4b6f50e0d0a
PreGros <tomas.z@volny.cz>
2023-04-03 10:16:57 +0200

    Added new properties to ftS class, functionality

--- main.py
+++ main.py
@@ -51,7 +51,4 @@ end_time = time.time()
 
 # Calculate elapsed time
 elapsed_time = end_time - start_time
-print("Elapsed time: ", elapsed_time) 
-
-
-#Ahojkda
+print("Elapsed time: ", elapsed_time)
